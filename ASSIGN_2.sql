c7a24e29e95ba3d5995a935a7a216fe1c76fa267
Rajesh bathula <bathularajesh837@gmail.com>
2026-07-17 08:55:08 -0400

    Update ASSIGN_2.sql

--- ASSIGN_2.sql
+++ ASSIGN_2.sql
@@ -33,4 +33,41 @@ SUM(LENGTH(LOWER(description))
 - LENGTH(REPLACE(LOWER(description),'u',''))) AS U_Count
 FROM film;
 
+- 4. Display the payments made by each customer
+--         1. Month wise
+--         2. Year wise
+--         3. Week wise
+SELECT * FROM sakila.customer;
+SELECT * FROM sakila.payment;
+SELECT  c.customer_id,   c.first_name,   c.last_name,
+    EXTRACT(YEAR FROM p.payment_date) AS payment_year,
+    EXTRACT(MONTH FROM p.payment_date) AS payment_month,
+	EXTRACT(WEEK FROM p.payment_date) AS payment_week,
+    SUM(p.amount) AS total_payment
+FROM sakila.customer c
+JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY 
+    c.customer_id, 
+    c.first_name, 
+    c.last_name, 
+    payment_year, 
+    payment_month,
+    payment_week
+ORDER BY 
+    c.customer_id, 
+    payment_year, 
+    payment_month,
+    payment_week;
 
+-- 5. Check if any given year is a leap year or not. You need not consider any table from sakila database. Write within the select query with hardcoded date
+
+
+-- 6. Display number of days remaining in the current year from today.
+SELECT DATEDIFF(CONCAT(YEAR(CURDATE()), '-12-31'), CURDATE()) AS days_remaining;
+
+-- 7. Display quarter number(Q1,Q2,Q3,Q4) for the payment dates from payment table.
+SELECT
+    payment_id,
+    payment_date,
+    CONCAT('Q', QUARTER(payment_date)) AS quarter_label
+FROM payment;
